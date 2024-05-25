a38ea3dbc0b3b701d81aebdd3e6d9423bb115482
Rechotek <maciej.jarek@gmail.com>
2024-05-25 10:52:28 +0200

    fff

--- game/events.c
+++ game/events.c
@@ -1,2 +1,27 @@
 #include "../so_long.h"
 
+void	move_player(t_game *game, short x_off, short y_off, char key) // off pochodzi od offset. W tym kontekscie oznacza odsuniecie od od punktu odniesienia
+{
+
+}
+
+// close_game odpowiada za zamkniecie okna z gra i zakonczenie dzialania programu
+// po zamknieciu okna zwalniana jest pamiec i program konczy dzialanie
+
+void	close_game(t_game *game)
+{
+	mlx_destroy_image(gane->mlx, game->txt.a); // mlx_destroy_image niszczy (usuwa) tekstury zaladowane do pamieci. Funkcja usuwa obrazy zaladowane do wskaznika
+	mlx_destroy_image(gane->mlx, game->txt.w);
+	mlx_destroy_image(gane->mlx, game->txt.c);
+	mlx_destroy_image(gane->mlx, game->txt.e);
+	mlx_destroy_image(gane->mlx, game->txt.pb);
+	mlx_destroy_image(gane->mlx, game->txt.pd);
+	mlx_destroy_image(gane->mlx, game->txt.pl);
+	mlx_destroy_image(gane->mlx, game->txt.pr);
+	mlx_destroy_image(gane->mlx, game->txt.t);
+	mlx_destroy_window(game->mlx, game->window); // niszczy (zamyka) okno utworzone przez minilibX
+	mlx_destroy_display(game->mlx); // zamyka wswietlacz w systemach UNIX. Jest uzywana do zwalniania zasobow zwiazanych z wyswietlaczem
+	free(game->mlx); // uwalnia pamiec zaalokowana dla game->mlx
+	free_map(game->map);
+	exit(0);
+}
--- game/start_game.c
+++ game/start_game.c
@@ -36,7 +36,22 @@ static void	init_textures(t_game *game)
 	game->txt.pr = mlx_xpm_file_to_image(game->mlx, "textures/pr.xpm", &(game->width), &(game->height));
 }
 
+// W tej funkcji okreslam co ma sie dziac po nacisnieciu danego klawisza na klawiaturze
 
+int	handle_key_events(int keycode, t_game *game)
+{
+	if (keycode == 65362) // jest to kod dla strzalki w gore
+		move_player(game, 0, -1, '↑');
+	if (keycode == 65364)
+		move_player(game, 0, 1, '↓');
+	if (keycode == 65361)
+		move_player(game, -1, 0, '←');
+	if (keycode == 65363)
+		move_player(game, 1, 0, '→');
+	if (keycode == 65307) // jest to kod dla ESC. Po jego nacisnieciu program ma sie zamknac
+		close_game(game);
+	return (0);
+}
 
 // Uruchamiam wszystkie funkcje, ktore sa niezbedne do rozpoczecia gry
 // HOOK - w kontekscie programowania to mechanizmy, ktore pozwalaja na zarejestrowanie
@@ -58,4 +73,6 @@ void	start_game(t_game game) // game jest kopia struktury t_game
 	init_textures(&game); // ta funkcja inicjalizuje tekstury. Odnosi sie ona do adresu &game, poniewaz tam sa zapisane niezbedne dane do inicjalizacji tekstur
 	fill_textures(game, 'w'); // ta funkcja wypelnia mape zainicjalizowanymi wyzej teksturami. Jako klucz jest podany 'w', poniewaz pierwszym polem do wypelnienia jest sciana, ktora oznaczylem jako 'w'
 	mlx_hook(game.window, 17, 0, close_game, &game); // obsluguje dzialanie na oknie ktore ma sie wydarzyc; przyjmuje wskaznik do okna dla ktorego rejestruje obsluge zdarzenia (game.window); typ zdarzenia, ktore chcemy obsluzyc, czyli co ma sie zadziac (17 - zamkniecie okna); maska zdarzen (0 - poniewaz nie potrzebuje dodatkowego filtrowania zdarzenia, do wylaczenia okna wystarczy 0); wskaznik do funkcji wykonujacej to zdarzenie (close_game); dane potrzebne do wykonania tej funkcji (&game)
+	mlx_key_hook(game.window, handle_key_events, &game); // obsluguje zdarzenia klawiaturowe dla danego okna; przyjmuje wskaznik na strukture reprezentujaca okno (game.window); funkcje gdzie zapisana jest obsluga klawiszy (handle_key_events); adres do danych, ktore sa niezbedne do wykonania funkcji (&game)
+
 }
